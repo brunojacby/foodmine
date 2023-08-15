8c8745565665c36bb4d6eb8b2bd6a4094071d40b
Bruno Jacby <brunojck@hotmail.com>
2023-08-16 00:15:10 +0100

    feat: adicionado pagina de carrinho de itens e total de items no header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { SearchComponent } from './components/partials/search/search.component';
 import { TagsComponent } from './components/partials/tags/tags.component';
 import { FoodPageComponent } from './components/pages/food-page/food-page.component';
 import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { TitleComponent } from './components/partials/title/title.component';
 
 
 @NgModule({
@@ -22,6 +23,7 @@ import { CartPageComponent } from './components/pages/cart-page/cart-page.compon
     TagsComponent,
     FoodPageComponent,
     CartPageComponent,
+    TitleComponent,
   ],
   imports: [
     BrowserModule,
