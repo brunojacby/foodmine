af603a9ef1f5b61c62a0d93510d293641bdcbfff
Bruno Jacby <brunojck@hotmail.com>
2023-08-18 11:42:43 +0100

    feat: connection to backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,30 +9,33 @@ app.use(cors({
 }));
 
 app.get("/api/foods", (req, res)=> {
-    res.send(sample_foods)
-});
+    res.send(sample_foods);
+})
 
 app.get("/api/foods/search/:searchTerm", (req, res) => {
     const searchTerm = req.params.searchTerm;
-    const foods = sample_foods.filter(food => food.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    const foods = sample_foods
+    .filter(food => food.name.toLowerCase()
+    .includes(searchTerm.toLowerCase()));
     res.send(foods)
 })
 
 
 app.get("/api/foods/tags", (req, res) => {
-    res.send(sample_tags)
+    res.send(sample_tags);
 })
 
 app.get("/api/foods/tag/:tagName", (req, res) => {
     const tagName = req.params.tagName;
-    const foods = sample_foods.filter(food => food.tags?.includes(tagName))
+    const foods = sample_foods
+    .filter(food => food.tags?.includes(tagName));
     res.send(foods)
 })
 
 app.get("/api/foods/:foodId", (req, res) => {
     const foodId = req.params.foodId;
     const food = sample_foods.find(food => food.id == foodId)
-    res.send(food)
+    res.send(food);
 })
 
 const port = 5000
