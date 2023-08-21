1a38197e2f78eb157da2678197bb5ef92cda6290
Bruno Jacby <brunojck@hotmail.com>
2023-08-21 21:12:09 +0100

    feat: adicionado register page

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -2,7 +2,9 @@ import { Router } from "express";
 import { sample_users } from "../data";
 import jwt from "jsonwebtoken"
 import asyncHandler from 'express-async-handler'
-import { UserModel } from "../models/user.model";
+import { User, UserModel } from "../models/user.model";
+import { HTTP_BAD_REQUEST } from "../constans/http_status";
+import bcrypt from 'bcryptjs'
 
 const router = Router();
 
@@ -23,13 +25,38 @@ router.post("/login", asyncHandler(
     async(req, res) => {    
     const { email, password } = req.body;
     //const user = sample_users.find(user => user.email === email && user.password === password);
-    const user = await UserModel.findOne({email, password})
-    if(user){
+    const user = await UserModel.findOne({email})
+    if(user && await bcrypt.compare(password, user.password)){
         res.send(generateTokenResponse(user))
     } else {
-        res.status(400).send("Username or password is not valid!")
+        res.status(HTTP_BAD_REQUEST).send("Username or password is not valid!")
     }
 }))
+
+router.post('/register', asyncHandler(
+    async(req, res) =>{
+        const {name, email, password, address} = req.body;
+        const user = await UserModel.findOne({email});
+        if(user){
+            res.status(HTTP_BAD_REQUEST).send('User is already exist, please login!');
+            return;
+        }
+
+        const encryptedPassword = await bcrypt.hash(password, 10)
+
+        const newUser:User = {
+            id:'',
+            name,
+            email: email.toLowerCase(),
+            password: encryptedPassword,
+            address,
+            isAdmin: false
+        }
+
+        const dbUser = await UserModel.create(newUser)
+        res.send(generateTokenResponse(dbUser))
+    }
+))
 
 const generateTokenResponse = (user:any) => {
     const token = jwt.sign({
