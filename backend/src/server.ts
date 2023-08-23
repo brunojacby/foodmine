7d0475fb6357bd7cb71ac718987cb35d3fd78bb3
Bruno Jacby <brunojck@hotmail.com>
2023-08-23 20:09:06 +0100

    fix: update url

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -22,11 +22,12 @@ app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
 
 app.use(express.static('public'));
-/*app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname,'public', 'index.html'))
-});*/
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public' + '/index.html'))
+});
 
-const port = process.env.PORT || 10000 || 5000;
+
+const port = process.env.PORT || 10000;
 app.listen(port, () => {
     console.log("Website served on http://localhost:" + port);
 });
