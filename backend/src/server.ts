7c5ff5a97d0b84bdc190b0d441942491a0080d63
Bruno Jacby <brunojck@hotmail.com>
2023-08-23 22:51:04 +0100

    refactor: link db

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -22,9 +22,9 @@ app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
 
 app.use(express.static('public'));
-app.get('*', (req, res) => {
+/*app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/public' + '/index.html'))
-});
+});*/
 
 
 const port = process.env.PORT || 5000;
