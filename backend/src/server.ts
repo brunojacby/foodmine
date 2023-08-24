936ca3537600d4c6d3660ebea3a8bb553d3f23f2
Bruno Jacby <brunojck@hotmail.com>
2023-08-24 17:55:16 +0100

    fix: backend folder

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -23,7 +23,7 @@ app.use("/api/orders", orderRouter);
 
 app.use(express.static('public'));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname,'public', 'index.html'))
+    res.sendFile(path.join(__dirname, 'index.html'))
 })
 
 
